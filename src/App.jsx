a5824219ebb2d185070f01aa1e9ff4a49ab0c12a
vasavadhanraj <vasavadhanraj26@gmail.com>
2024-11-22 10:22:18 +0530

    New Files

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch} from 'react-redux'
+import './App.css'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import {Header, Footer} from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Todo: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+{
+  /* <p>Sample <strong>article</strong></p> 
+ test article
+
+673c12050015125e3332 UserID
+673c133c000580e18c71 imageID
+
+*/}
